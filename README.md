6e239aafe5ccae00fe00fd1862c553608d7d7ab5
wbytts <wbytts@163.com>
2022-08-03 10:49:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue2-vue-cli-ocqjdf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue2-vue-cli-ocqjdf)
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>
+      <el-button type="primary">按钮</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
